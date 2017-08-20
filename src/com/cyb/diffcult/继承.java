f9d75d4683edd169265c4708e986dc5549c686c5
iechenyb <zzuchenyb@sina.com>
2017-08-20 17:41:28 +0800

    更新

--- src/com/cyb/diffcult/继承.java
+++ src/com/cyb/diffcult/继承.java
@@ -1,32 +1,46 @@
 package com.cyb.diffcult;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class 继承 {
+
+@SuppressWarnings("static-access")
 public static void main(String[] args) {
-	System.out.println(new Parent()==new Child());
 	Child c = new Child();
 	c.print2(); //protectedl可见
 	//c.print1();//不可见 私有方法
 	c.print3();
+	c.staticMethod();//执行重写的静态方法
 	Parent p = new Child();
 	p.print2(); //protectedl可见
 	//p.print1();//不可见 私有方法,只能被其他方法使用，不能被类实例使用
 	p.print3();
+	p.staticMethod();//重写的静态方法不可见，调用父类自己的静态方法。
 }
 }
 class Parent{
+	 static Log log = LogFactory.getLog(Parent.class);
 	private void print1(){
-		System.out.println(" parent private method!");
+		log.info(" parent private method!");
 	}
 	protected void print2(){
-		System.out.println(" parent protected method!");
+		log.info(" parent protected method!");
 	}
 	 void print3(){
 		print1();//私有方法只能在类内部使用。
-		System.out.println(" parent default method!");
+		log.info(" parent default method!");
 	}
+	 static void  staticMethod(){
+		 log.info(" parent default static method!");
+	 }
 }
 class Child extends Parent{
+	static Log log = LogFactory.getLog(Child.class);
 	void print3(){
-		System.out.println(" parent default method overide!");
+		log.info(" child default method overide!");
+	}
+	static void  staticMethod(){
+		log.info(" child default static method overide!");
 	}
 }
