8465cd527ca80e937888e080078d504829fcc582
iechenyb <zzuchenyb@sina.com>
2017-08-25 16:47:11 +0800

    log4j日志重写

--- src/com/cyb/log/MyLogUtil.java
+++ src/com/cyb/log/MyLogUtil.java
@@ -4,10 +4,10 @@ import org.apache.commons.logging.LogFactory;
 public class MyLogUtil {
 		static Log log =LogFactory.getLog(MyLogUtil.class);
 		public static void main(String[] args) throws Exception {
-			log.debug("debug级别的日志输出");  
-	        log.debug("debug级别的日志输出1");  
-	        log.debug("debug级别的日志输出2");  
-	        log.debug("debug级别的日志输出3"); 
-	        throw new Exception("ERROR ");
+			for(int i=0;i<10000*10;i++){
+				log.debug("debug级别的日志输出"+i); 
+				log.info("debug级别的日志输出"+i); 
+			}
+	        //throw new Exception("ERROR ");
 		} 
 }
