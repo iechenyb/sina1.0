5827d6a24f910e70f83f4ae207fe38089265a0fb
iechenyb <zzuchenyb@sina.com>
2017-08-30 11:16:06 +0800

    更新三目运算符

--- src/com/cyb/diffcult/structurecontrol/IF.java
+++ src/com/cyb/diffcult/structurecontrol/IF.java
@@ -8,9 +8,13 @@ import org.apache.commons.logging.LogFactory;
  */
 public class IF {
 	static Log log = LogFactory.getLog(IF.class);
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+		System.out.println(getTrue("left")==getFalse("right"));
 		if(getTrue("left")||getFalse("right")){}//left
+		System.out.println();
 		if(getFalse("left")||getTrue("right")){}//left+right
+		System.out.println();
 		int a=6,b=8;
 		boolean c = false;
 		//c = 6<8||a++==b--;
@@ -18,13 +22,16 @@ public class IF {
 		System.out.println(c+","+a+","+b);
 		//从左到右解析，短路判断
 		c=getFalse("l1")||getFalse("l2")||getTrue("r2")||getFalse("r1");
+		System.out.println();
+		boolean d = (getTrue("1") == getFalse("2") )? getTrue("3") : getFalse("4");
+		//1,2,4
 	}
 	public static  boolean getTrue(String str){
-		log.info(str);
+		System.out.print(str+",");
 		return true;
 	}
 	public static boolean getFalse(String str){
-		log.info(str);
+		System.out.print(str+",");
 		return false;
 	}
 	
