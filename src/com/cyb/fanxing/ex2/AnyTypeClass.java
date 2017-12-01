4c887de2d60f71e3c0e324b5bc68f7745160826d
iechenyb <zzuchenyb@sina.com>
2017-12-01 10:33:49 +0800

    继承父类泛型需要重写方法

---
+++ src/com/cyb/fanxing/ex2/AnyTypeClass.java
@@ -0,0 +1,14 @@
+package com.cyb.fanxing.ex2;
+
+/**
+ *作者 : iechenyb<br>
+ *类描述: 说点啥<br>
+ *创建时间: 2017年12月1日
+ */
+public class AnyTypeClass<T> {
+   public T getVar(){
+	   return null;
+   }
+}
+
+
